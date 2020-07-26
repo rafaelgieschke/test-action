b57a25824c502ace861401dee5ad5bdeb6ee8128
Rafael Gieschke <rafael@gieschke.de>
2020-07-26 15:09:53 +0200

    .

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,16 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import {preserveShebangs} from "rollup-plugin-preserve-shebangs";
+import { preserveShebangs } from "rollup-plugin-preserve-shebangs";
 
 export default {
     input: "index.js",
-    output: {
-        file: "index.dist.mjs",
-        format: "esm",
-    },
+    output: [
+        {
+            file: "index.dist.mjs",
+            format: "esm",
+        },
+        { file: "index.dist.cjs.js", format: "cjs" },
+    ],
     plugins: [
         preserveShebangs(),
         nodeResolve({
